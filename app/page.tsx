5a4665311213ff346d853440865e896fc45b97e0
Ian Valentino <130348417+ivrequenajerez@users.noreply.github.com>
2024-07-27 16:57:57 +0200

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import MyHeader from "@/components/MyHeader";
+import SobreMi from "@/components/SobreMi";
 
 export default function Home() {
   return (
     <main>
       <MyHeader />
       <Hero/>
+      <SobreMi/>
     </main>
   );
 }
---
+++ components/SobreMi.tsx
@@ -0,0 +1,15 @@
+import { motion } from "framer-motion"
+
+const SobreMi = () => {
+  return (
+    <section id="sobre-mi">
+        <h2>Elemento que va a aparecer</h2>
+        <h2>Elemento que va a aparecer</h2>
+        <h2>Elemento que va a aparecer</h2>
+        <h2>Elemento que va a aparecer</h2>
+        <h2>Elemento que va a aparecer</h2>
+    </section>
+  )
+}
+
+export default SobreMi
