c6478a540186d270435882836ea0089e0b59e864
Ian Valentino <130348417+ivrequenajerez@users.noreply.github.com>
2024-07-28 04:16:24 +0200

    Animación básica al scrollear aparece el texto

--- components/SobreMi.tsx
+++ components/SobreMi.tsx
@@ -19,6 +19,34 @@ const SobreMi = () => {
           </p>
         </div>
       </ScrollAppear>
+      <ScrollAppear>
+        <div className="flex flex-col items-center justify-center h-96 px-10">
+          <h2 className="text-7xl font-bold w-1/2 text-center py-5">
+            Creatividad a otro nivel
+          </h2>
+          <p className="text-lg font-medium w-1/2 text-center">
+            Aprovechando todo mi potencial profesional para luego impulsar las{" "}
+            <strong>ideas</strong> más brillantes a su mayor expresión. Me
+            apasiona trabajar en proyectos creativos, transformando actividades
+            cotidianas en experiencias fáciles, cómodas y agradables. ¡Hagamos
+            algo increíble juntos!
+          </p>
+        </div>
+      </ScrollAppear>
+      <ScrollAppear>
+        <div className="flex flex-col items-center justify-center h-96 px-10">
+          <h2 className="text-7xl font-bold w-1/2 text-center py-5">
+            Creatividad a otro nivel
+          </h2>
+          <p className="text-lg font-medium w-1/2 text-center">
+            Aprovechando todo mi potencial profesional para luego impulsar las{" "}
+            <strong>ideas</strong> más brillantes a su mayor expresión. Me
+            apasiona trabajar en proyectos creativos, transformando actividades
+            cotidianas en experiencias fáciles, cómodas y agradables. ¡Hagamos
+            algo increíble juntos!
+          </p>
+        </div>
+      </ScrollAppear>
     </section>
   );
 };
