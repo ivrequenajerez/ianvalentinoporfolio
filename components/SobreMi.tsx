7e3ee60bcb6249b8b3e15d8bde065097a7e1e781
Ian Valentino <130348417+ivrequenajerez@users.noreply.github.com>
2024-08-02 13:45:02 +0200

    -

--- components/SobreMi.tsx
+++ components/SobreMi.tsx
@@ -8,7 +8,7 @@ const SobreMi = () => {
       <ScrollAppear>
         <div className="flex flex-col items-center justify-center h-96 px-10">
           <h2 className="text-7xl font-bold w-1/2 text-center py-5">
-            Creatividad a otro nivel
+            Tus ideas, las llevo a la realidad
           </h2>
           <p className="text-lg font-medium w-1/2 text-center">
             Aprovechando todo mi potencial profesional para luego impulsar las{" "}
