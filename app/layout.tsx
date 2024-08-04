c26dc301fb92713d87795cdc2b0cfa49a40ff718
Ian Valentino <130348417+ivrequenajerez@users.noreply.github.com>
2024-08-04 13:29:08 +0200

    favicon añadido

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,11 @@ const montserrat = Montserrat({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Porfolio de Ian Requena",
   description: "Trabajos y Conocimientos de Ian",
+  icons:{
+    icon:['/favicon.ico?v=4'],
+    apple:['/apple-touch-icon.png?v=4'],
+    shortcut:['/apple-touch-icon.png']
+  }
 };
 
 export default function RootLayout({
