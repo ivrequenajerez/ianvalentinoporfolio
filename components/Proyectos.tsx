94c66a33176d12ac45f0e291407c22175e11295d
Ian Valentino <130348417+ivrequenajerez@users.noreply.github.com>
2024-08-04 03:35:57 +0200

    -

--- components/Proyectos.tsx
+++ components/Proyectos.tsx
@@ -29,16 +29,68 @@ const Proyectos = () => {
             <GlareCard className="relative flex flex-col items-center justify-center">
               <img
                 className="h-full w-full absolute inset-0 object-cover"
-                src="https://images.unsplash.com/photo-1512618831669-521d4b375f5d?q=80&w=3388&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                src="/eazy.png"
               />
-
+              <div className="h-full w-full absolute inset-0 bg-black opacity-80"></div>
+              <div className="flex flex-col items-start justify-end py-8 px-6 z-10">
+                <p className="font-bold text-white text-lg">EazyChef</p>
+                <p className="font-normal text-base text-neutral-200 mt-4">
+                  Aplicación multiplataforma para gestión de almacén de un
+                  restaurante. React Native, NativeWind, TailWind, NoSQL, Expo, JavaScript.
+                </p>
+              </div>
+            </GlareCard>
+          </GlareCard>
+          <GlareCard className="flex flex-col items-center justify-center">
+            <GlareCard className="relative flex flex-col items-center justify-center">
+              <img
+                className="h-full w-full absolute inset-0 object-cover"
+                src="/mila.png"
+              />
+              <div className="h-full w-full absolute inset-0 bg-black opacity-80"></div>
+              <div className="flex flex-col items-start justify-end py-8 px-6 z-10">
+                <p className="font-bold text-white text-lg">
+                  Mila Peluquería Unisex
+                </p>
+                <p className="font-normal text-base text-neutral-200 mt-4">
+                  Sitio Web personalizado para hacer reservas en Centro de
+                  Belleza. CMS WordPress, PHP, Html, CSS.
+                </p>
+              </div>
+            </GlareCard>
+          </GlareCard>
+          <GlareCard className="flex flex-col items-center justify-center">
+            <GlareCard className="relative flex flex-col items-center justify-center">
+              <img
+                className="h-full w-full absolute inset-0 object-cover"
+                src="/asystem.png"
+              />
+              <div className="h-full w-full absolute inset-0 bg-black opacity-80"></div>
+              <div className="flex flex-col items-start justify-end py-8 px-6 z-10">
+                <p className="font-bold text-white text-lg">
+                  Asystem: Webs y Diseño Gráfico
+                </p>
+                <p className="font-normal text-base text-neutral-200 mt-4">
+                  Sitio Web personalizado, diseños modernos e implementación de
+                  funcionalidades complejas. CMS WordPress, PHP, Html, CSS.
+                </p>
+              </div>
+            </GlareCard>
+          </GlareCard>
+          <GlareCard className="flex flex-col items-center justify-center">
+            <GlareCard className="relative flex flex-col items-center justify-center">
+              <img
+                className="h-full w-full absolute inset-0 object-cover"
+                src="/delivuconnect.png"
+              />
+              <div className="h-full w-full absolute inset-0 bg-black opacity-80"></div>
               <div className="flex flex-col items-start justify-end py-8 px-6 z-10">
                 <p className="font-bold text-white text-lg">
                   Delivuconnect: Directorio Empresarial
                 </p>
                 <p className="font-normal text-base text-neutral-200 mt-4">
                   Directorio web con empresas de diversos sectores. Buscador
-                  personalizado. CMS Wordpress.
+                  personalizado. CMS WordPress, PHP, Html, CSS.
                 </p>
               </div>
             </GlareCard>
@@ -52,65 +104,11 @@ const Proyectos = () => {
 
               <div className="flex flex-col items-start justify-end py-8 px-6 z-10">
                 <p className="font-bold text-white text-lg">
-                  Mila Peluquería Unisex
+                  Pazzi Per La Pizza: Sitio Web
                 </p>
                 <p className="font-normal text-base text-neutral-200 mt-4">
-                  Sitio Web personalizado para hacer reservas en Centro de
-                  Belleza. CMS WordPress, PHP, Html, CSS.
-                </p>
-              </div>
-            </GlareCard>
-          </GlareCard>
-          <GlareCard className="flex flex-col items-center justify-center">
-            <GlareCard className="relative flex flex-col items-center justify-center">
-              <img
-                className="h-full w-full absolute inset-0 object-cover"
-                src="https://images.unsplash.com/photo-1512618831669-521d4b375f5d?q=80&w=3388&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              />
-
-              <div className="flex flex-col items-start justify-end py-8 px-6 z-10">
-                <p className="font-bold text-white text-lg">
-                  Mila Peluquería Unisex
-                </p>
-                <p className="font-normal text-base text-neutral-200 mt-4">
-                  Sitio Web personalizado para hacer reservas en Centro de
-                  Belleza. CMS WordPress, PHP, Html, CSS.
-                </p>
-              </div>
-            </GlareCard>
-          </GlareCard>
-          <GlareCard className="flex flex-col items-center justify-center">
-            <GlareCard className="relative flex flex-col items-center justify-center">
-              <img
-                className="h-full w-full absolute inset-0 object-cover"
-                src="https://images.unsplash.com/photo-1512618831669-521d4b375f5d?q=80&w=3388&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              />
-
-              <div className="flex flex-col items-start justify-end py-8 px-6 z-10">
-                <p className="font-bold text-white text-lg">
-                  Mila Peluquería Unisex
-                </p>
-                <p className="font-normal text-base text-neutral-200 mt-4">
-                  Sitio Web personalizado para hacer reservas en Centro de
-                  Belleza. CMS WordPress, PHP, Html, CSS.
-                </p>
-              </div>
-            </GlareCard>
-          </GlareCard>
-          <GlareCard className="flex flex-col items-center justify-center">
-            <GlareCard className="relative flex flex-col items-center justify-center">
-              <img
-                className="h-full w-full absolute inset-0 object-cover"
-                src="https://images.unsplash.com/photo-1512618831669-521d4b375f5d?q=80&w=3388&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              />
-
-              <div className="flex flex-col items-start justify-end py-8 px-6 z-10">
-                <p className="font-bold text-white text-lg">
-                  Mila Peluquería Unisex
-                </p>
-                <p className="font-normal text-base text-neutral-200 mt-4">
-                  Sitio Web personalizado para hacer reservas en Centro de
-                  Belleza. CMS WordPress, PHP, Html, CSS.
+                  Sitio Web personalizado para hacer reservas y ver Carta en
+                  línea. CMS WordPress, PHP, Html, CSS.
                 </p>
               </div>
             </GlareCard>
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -7,7 +7,7 @@ const Skills: React.FC = () => {
       <div className="h-[40rem] w-full flex items-center justify-center">
         <PinContainer
           title="Html, CSS, JavaScript, TypeScript"
-          href="https://twitter.com/mannupaaji"
+          href="https://github.com/ivrequenajerez"
         >
           <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem]">
             <h3 className="max-w-xs !pb-2 !m-0 font-bold text-base text-slate-100">
@@ -22,7 +22,7 @@ const Skills: React.FC = () => {
         </PinContainer>
         <PinContainer
           title="Java, SpringBoot, JavaScript, React Native"
-          href="https://twitter.com/mannupaaji"
+          href="https://github.com/ivrequenajerez"
         >
           <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem]">
             <h3 className="max-w-xs !pb-2 !m-0 font-bold text-base text-slate-100">
@@ -38,7 +38,7 @@ const Skills: React.FC = () => {
         </PinContainer>
         <PinContainer
           title="Elementor, Divi, WooCommerce"
-          href="https://twitter.com/mannupaaji"
+          href="https://github.com/ivrequenajerez"
         >
           <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem]">
             <h3 className="max-w-xs !pb-2 !m-0 font-bold text-base text-slate-100">
